02802e11ba38ffa323d990b73e0453a39a726928
buddi56 <30726378+buddi56@users.noreply.github.com>
2020-01-05 22:47:02 +0530

    Debloot bat file.

---
+++ DeBloot.bat
@@ -0,0 +1,36 @@
+@title Debloot Realme 1
+@echo off
+adb start-server
+cls
+echo.
+echo ---------------------------------------------------------------------------------------------------------------
+echo A small file for removing blootware from my phone.
+echo Press any key to continue.
+echo ---------------------------------------------------------------------------------------------------------------
+pause >nul
+echo Waiting for Device.
+adb wait-for-device
+adb uninstall  --user 0 com.android.stk
+adb uninstall  --user 0 com.google.android.tts
+adb uninstall  --user 0 com.google.android.googlequicksearchbox
+adb uninstall  --user 0 com.coloros.smartdrive
+adb uninstall  --user 0 com.google.android.marvin.talkback
+adb uninstall  --user 0 com.android.browser
+adb uninstall  --user 0 com.facebook.system
+adb uninstall  --user 0 com.facebook.appmanager
+adb uninstall  --user 0 com.facebook.services
+adb uninstall  --user 0 com.coloros.pictorial
+adb uninstall  --user 0 com.mediatek.mtklogger
+adb uninstall  --user 0 com.coloros.cloud
+adb uninstall  --user 0 com.oppo.logkit
+adb uninstall  --user 0 com.nearme.statistics.rom
+adb uninstall  --user 0 com.oppo.operationManual
+adb uninstall  --user 0 com.coloros.backuprestore
+adb uninstall  --user 0 com.coloros.phonemanager
+adb uninstall  --user 0 com.google.android.youtube
+adb uninstall  --user 0 com.realme.logtool
+adb shell pm disable-user --user 0 com.coloros.gamespace
+adb shell pm disable-user --user 0 com.nearme.gamecenter
+echo Done! Press any key to continue.
+pause >nul              
+exit
