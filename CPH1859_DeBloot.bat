fa6e17efeb11fd3c2dab6cbebdefe1d708c446ca
buddi56 <30726378+buddi56@users.noreply.github.com>
2020-07-26 21:42:45 +0530

    add some more blootware apps.

--- CPH1859_DeBloot.bat
+++ CPH1859_DeBloot.bat
@@ -29,12 +29,24 @@ adb uninstall  --user 0 com.coloros.phonemanager
 adb uninstall  --user 0 com.google.android.youtube
 adb uninstall  --user 0 com.realme.logtool
 adb uninstall  --user 0 com.oppo.qualityprotect
+adb uninstall  --user 0 com.oppo.market
 adb uninstall  --user 0 com.oppo.partnerbrowsercustomizations
+adb uninstall  --user 0 com.google.android.apps.photos
 adb uninstall  com.google.android.apps.tachyon
 adb uninstall  com.google.android.videos
 adb uninstall  com.google.android.music
+adb uninstall  --user 0 com.coloros.avastofferwall
+adb uninstall  --user 0 com.coloros.childrenspace
+adb uninstall  --user 0 com.coloros.securepay
+adb uninstall  --user 0 com.nearme.atlas
+adb uninstall  --user 0 com.trustonic.teeservice
+adb uninstall  --user 0 com.google.android.keep
+adb uninstall  --user 0 com.realme.securitycheck
+adb uninstall  --user 0 com.android.fmradio
+adb uninstall  --user 0 com.coloros.healthcheck
+adb uninstall  --user 0 com.oppo.market
 adb shell pm disable-user --user 0 com.coloros.gamespace
 adb shell pm disable-user --user 0 com.nearme.gamecenter
 echo Done! You can now exit, press a key.
-pause >nul              
+pause >nul
 exit
